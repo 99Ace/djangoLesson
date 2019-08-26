a7df5459b56bf8a3cbbbcbe1a6ac67640942b197
Ubuntu <ubuntu@ip-172-31-86-16.ec2.internal>
2019-08-26 07:36:35 +0000

    First commit

---
+++ todos/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+# declare a class named "Item"
+class Item(models.Model):
+    name = models.CharField(max_length=30, blank=False)
+    done = models.BooleanField(blank=False, default=False)
+    ongoing = models.BooleanField(blank=False, default=False)
+    completed_at = models.DateField(blank=True, null=True, default=None)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tag')
+    assign = models.ForeignKey('Arrow', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+        
+class Category(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    def __str__(self):
+        return self.name
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    def __str__(self):
+        return self.name    
+        
+class Arrow(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    def __str__(self):
+        return self.name
